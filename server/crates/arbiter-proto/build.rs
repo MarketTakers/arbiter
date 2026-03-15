013af7e65fc776b63b86a793c5c1487f1577e5f1
hdbg <httpdebugger@protonmail.com>
2026-03-15 16:42:03 +0100

    fix(server): remove useless vendored protoc

--- server/crates/arbiter-proto/build.rs
+++ server/crates/arbiter-proto/build.rs
@@ -3,12 +3,6 @@ use tonic_prost_build::configure;
 static PROTOBUF_DIR: &str = "../../../protobufs";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if std::env::var("PROTOC").is_err() {
-        println!("cargo:warning=PROTOC environment variable not set, using vendored protoc");
-        let protoc = protoc_bin_vendored::protoc_bin_path().unwrap();
-        unsafe { std::env::set_var("PROTOC", protoc) };
-    }
-
     println!("cargo::rerun-if-changed={PROTOBUF_DIR}");
 
     configure()
